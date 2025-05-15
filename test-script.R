fa80906117229c2120d27f539ce775a3b8a60a29
chloecarlstrom <chloemcarlstrom@gmail.com>
2025-05-15 16:17:20 -0700

    changing colors in visualization

--- test-script.R
+++ test-script.R
@@ -3,5 +3,8 @@ library(lterdatasampler)
 
 ggplot(data = pie_crab,
        aes(x = latitude,
-           y = size)) +
-  geom_point()
+           y = size,
+           color = latitude)) +
+  geom_point() +
+  scale_color_gradient(low = "#378922",
+                       high = "#782096")
